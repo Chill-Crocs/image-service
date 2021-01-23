92c9842241cb910bd0318679f7df76d187d5b6ba
Michael Gunyan <mrgunyan@gmail.com>
2021-01-23 13:48:55 -0700

    fixed main carousel buttons

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -152,21 +152,41 @@ class App extends React.PureComponent {
 
   render() {
     const {
-      images, main, fav, thumbClass, modalThumbClass,
-      showModal, modalMain,
+      images, main, thumbClass, modalThumbClass,
+      showModal, modalMain, favorite,
     } = this.state;
+    let favoriteButton;
+    let fav;
+    if (favorite) {
+      favoriteButton = <path d="M16.5,3A6.953,6.953,0,0,0,12,5.051,6.912,6.912,0,0,0,7.5,3C4.364,3,2,5.579,2,9c0,5.688,8.349,12,10,12S22,14.688,22,9C22,5.579,19.636,3,16.5,3Z" />;
+      fav = 'image-fav';
+    } else {
+      favoriteButton = <path d="M12,21C10.349,21,2,14.688,2,9,2,5.579,4.364,3,7.5,3A6.912,6.912,0,0,1,12,5.051,6.953,6.953,0,0,1,16.5,3C19.636,3,22,5.579,22,9,22,14.688,13.651,21,12,21ZM7.5,5C5.472,5,4,6.683,4,9c0,4.108,6.432,9.325,8,10,1.564-.657,8-5.832,8-10,0-2.317-1.472-4-3.5-4-1.979,0-3.7,2.105-3.721,2.127L11.991,8.1,11.216,7.12C11.186,7.083,9.5,5,7.5,5Z" />;
+      fav = 'image-notFav';
+    }
     return (
       <div className="image-grid">
 
         <Thumbs thumbClass={thumbClass} images={images} thumbs={this.thumbClick} />
 
         <div className="image-col2">
-          <span onClick={this.leftClick} name="button" role="button" tabIndex={0} onKeyUp={this.leftClick}><img className="image-left" alt="noimage" src="left.svg" /></span>
+          <button onClick={this.leftClick} className="image-left" type="button">
+            <svg viewBox="0 0 24 24">
+              <path d="M16,21a0.994,0.994,0,0,1-.664-0.253L5.5,12l9.841-8.747a1,1,0,0,1,1.328,1.494L8.5,12l8.159,7.253A1,1,0,0,1,16,21Z" />
+            </svg>
+          </button>
           <span onClick={this.mainClick} name="main" role="button" tabIndex={0} onKeyUp={this.mainClick}><img id="main" name="main" className="image-main" alt="noimage" src={images[main]} /></span>
-          <span onClick={this.rightClick} role="button" tabIndex={0} onKeyUp={this.rightClick}><img className="image-right" alt="noimage" src="right.svg" /></span>
+          <button onClick={this.rightClick} className="image-right" type="button">
+            <svg viewBox="0 0 24 24">
+              <path d="M8,21a1,1,0,0,1-.664-1.747L15.5,12,7.336,4.747A1,1,0,0,1,8.664,3.253L18.5,12,8.664,20.747A0.994,0.994,0,0,1,8,21Z" />
+            </svg>
+          </button>
+          <button onClick={this.changeFav} className={fav} type="button">
+            <svg viewBox="0 0 24 24">
+              {favoriteButton}
+            </svg>
+          </button>
         </div>
-
-        <span onClick={this.changeFav} name="fav" role="button" tabIndex={0} onKeyUp={this.changeFav}><img className="image-fav" alt="noimage" src={fav} /></span>
 
         <Modal
           thumbClass={modalThumbClass}
